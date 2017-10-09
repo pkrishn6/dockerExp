090329271e7da1cca97c6da21279fa654f95caf9
Prasanna kumar Krishnamurthy <pkumar@snaproute.com>
2017-10-09 15:32:47 -0700

    Generalize setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 ./cleanup.sh
+pushd $(pwd)
 cd $SR_CODE_BASE/snaproute/src/mino-masterd
 make
 cd $SR_CODE_BASE/snaproute/src/mino-testd
 make
-cd ~/dockerExp/
-cp $out/masterd .
-cp $out/testd .
-sudo docker build --rm -t pkumar:serf_master -f Dockerfile.master .
-sudo docker build --rm -t pkumar:serf_test -f Dockerfile.test .
+popd
+cp $SR_CODE_BASE/snaproute/src/out/bin/masterd .
+cp $SR_CODE_BASE/snaproute/src/out/bin/testd .
+sudo docker build --rm -t $USER:serf_master -f Dockerfile.master .
+sudo docker build --rm -t $USER:serf_test -f Dockerfile.test .
